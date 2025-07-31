fae0bb9834220c83a2daa66b0956e9399a769416
sinedindnd@hotmail.com <sinedindnd@hotmail.com>
2025-07-30 21:30:34 -0500

    cambios Program y appsettings.json

--- Program.cs
+++ Program.cs
@@ -1,14 +1,22 @@
+using API_Productos.Data;
+using API_Productos.ProductoMappers;
+using API_Productos.Repositorio;
+using API_Productos.Repositorio.IRepositorio;
 using Microsoft.EntityFrameworkCore;
-using ProductosApiRest.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AplicationDbContext>(opciones => 
+    opciones.UseSqlServer(builder.Configuration.GetConnectionString("conexionSql"))
+);
+
+builder.Services.AddScoped<IProductorepositorio, ProductoRepositorio>();
+
+// Agregar AutoMapper
+builder.Services.AddAutoMapper(typeof(ProductosMappers));
 
 builder.Services.AddControllers();
-// Configurar el contexto con InMemory
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseInMemoryDatabase("BaseDeDatosEnMemoria"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
